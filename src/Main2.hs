7769e0327a7213818cf79f634dd55b572b489e35
Carlos Gomez <carliros.g@gmail.com>
2021-06-16 19:48:40 -0400

    compile parser

--- src/Main2.hs
+++ src/Main2.hs
@@ -1,5 +1,15 @@
 module Main where
 
+import SistemaL
+import ParserLS ( parseFileSistemasL )
+
+readDataBase 
+    = do lss <- parseFileSistemasL "./db/db.sl"
+         let defaultLS = SistemaL "Single Line" [Simbolo "F"] [Simbolo "F"] [(Simbolo "F",[Simbolo "F"])]
+         return $ defaultLS : lss
+
 main :: IO ()
 main = do
   putStrLn "Hello world"
+  dt <- readDataBase
+  putStrLn $ show dt
--- src/ParserLS.hs
+++ src/ParserLS.hs
@@ -3,51 +3,20 @@ module ParserLS where
 
 import Text.ParserCombinators.UU
 import Text.ParserCombinators.UU.BasicInstances
-import Text.ParserCombinators.UU.Utils
+import Text.ParserCombinators.UU.Utils ( runParser, lexeme, pDigit, pLetter, pSpaces, pSymbol )
 import SistemaL
 import Data.Char
 import Data.Maybe
 import Data.List
 import Text.Printf
 
--- %% Parser Interface %%
---execParser' :: Parser a -> String -> (a, [Error LineColPos])
---execParser' p = parse ((,) <$> p <*> pEnd) . createStr (LineColPos 0 0 0)
-
-runParser' :: String -> Parser a -> String -> IO a
-runParser' inputName p s
-    = do let (a,b) = execParser p s 
-         if null b
-          then return a
-          else printf "Failed parsing '%s' :\n%s" inputName (showErrors s b) >> return a
-    where showErrors :: String -> [Error LineColPos] -> String
-          showErrors s errs = foldr (\e r -> "-> " ++ (pruneError s e) ++ r) "\n" errs
-          pruneError :: String -> Error LineColPos -> String
-          pruneError _ (DeletedAtEnd x    ) = printf "\nUnexpected '%s' at end." x
-          pruneError s (Inserted x pos exp) = prettyError s exp pos ++ printf "\n\t Corrected with inserting: '%s'" x
-          pruneError s (Deleted  x pos exp) = prettyError s exp pos ++ printf "\n\t Corrected with deleting: %s" x
-
-          prettyError :: String -> [String] -> LineColPos -> String
-          prettyError s exp p@(LineColPos line c abs) = printf "\nExpected %s at %s :\n%s\n%s\n%s\n"
-                                                           (show_expecting p exp)
-                                                           (show p)
-                                                           aboveString
-                                                           inputFrag
-                                                           belowString
-                             where
-                                s' = map (\c -> if c=='\n' || c=='\r' || c=='\t' then ' ' else c) s
-                                aboveString = replicate 30 ' ' ++ "v"
-                                belowString = replicate 30 ' ' ++ "^"
-                                inputFrag   = replicate (30 - c) ' ' ++ (take 71 $ drop (c - 30) s')
-
-
 parseFileSistemasL :: FilePath -> IO [SistemaL]
 parseFileSistemasL file 
     = do input <- readFile file
-         res <- runParser' file pSistemasL input
+         let res = runParser file pSistemasL input :: [SistemaL]
          mbres <- mapM verificar res
          return $ catMaybes mbres
-    where verificar sl = case (testSistemaL sl) of
+    where verificar sl = case testSistemaL sl of
                              Left errs -> do mapM_ putStrLn errs
                                              return Nothing
                              Right sl  -> return $ Just sl
@@ -88,7 +57,7 @@ pNombre = lexeme $ unwords <$> pList1Sep_ng pSpaces pSimpleWord
 
 pSLSimbolo :: Parser Simbolo
 pSLSimbolo = lexeme $ Simbolo <$> pMunch funchar
-    where funchar c = not $ elem c "[]{},; \n\r\t"
+    where funchar c = c `notElem` "[]{},; \n\r\t"
 
 -- auxiliar parsers
 pSimpleWord :: Parser String
